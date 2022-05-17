4c2e1a8d8bd913119838f14b7d15b7c39043059b
Akhil Khan <akhilkhan0786khan@gmail.com>
2022-05-17 18:37:22 +0530

    Counter app in react

---
+++ src/cnter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState } from "react";
+
+const Counter = () => {
+    const [count,setCount] = useState(0);
+
+    if(count%2==0)
+    {
+       var style1 ={
+        color:'red'
+        }
+        console.log("even")
+    }else{
+        var style1 ={
+            color:'green'
+            }
+        console.log("Odd")
+    }
+
+    return (
+        <div>
+            <h1 style={style1}>Counter App: {count}</h1>
+            <button onClick={()=> setCount(count-1)}>Decrement</button>
+            <button onClick={()=> setCount(count+1)}>Increment</button>
+            <button onClick={()=> setCount(count*2)}>Double</button>
+            <button onClick={()=> setCount(count-count)}>Reset</button>
+        </div>
+        
+    )
+}
+
+export default Counter;
